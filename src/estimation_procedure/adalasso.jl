e684b9466868d85194a8e4f96bc4d59a30215246
andreramosfc <andreramosfdc@gmail.com>
2023-11-22 10:39:28 -0300

    📝first documentation

--- src/estimation_procedure/adalasso.jl
+++ src/estimation_procedure/adalasso.jl
@@ -1,3 +1,24 @@
+"""
+    fit_adalasso(Estimation_X::Matrix{Tl}, estimation_y::Vector{Fl}, α::Float64,
+                 hyperparameter_selection::String,
+                 components_indexes::Dict{String, Vector{Int64}},
+                 adalasso_coef::Float64, select_exogenous::Bool)::Tuple{Vector{Float64}, Vector{Float64}} where {Tl, Fl}
+
+    Fits an Adaptive Lasso (AdaLasso) regression model to the provided data and returns coefficients and residuals.
+
+    # Arguments
+    - `Estimation_X::Matrix{Tl}`: Matrix of predictors for estimation.
+    - `estimation_y::Vector{Fl}`: Vector of response values for estimation.
+    - `α::Float64`: Elastic net control factor between ridge (α=0) and lasso (α=1) (default: 0.1).
+    - `hyperparameter_selection::String`: Information Criteria method for hyperparameter selection (default: aic).
+    - `components_indexes::Dict{String, Vector{Int64}}`: Dictionary containing indexes for different components.
+    - `adalasso_coef::Float64`: AdaLasso adjustment coefficient (default: 0.1).
+    - `select_exogenous::Bool`: Flag for selecting exogenous variables. When false the penalty factor for these variables will be set to 0.
+
+    # Returns
+    - `Tuple{Vector{Float64}, Vector{Float64}}`: Tuple containing coefficients and residuals of the fitted AdaLasso model.
+
+"""
 function fit_adalasso(Estimation_X::Matrix{Tl}, estimation_y::Vector{Fl}, α::Float64, 
                         hyperparameter_selection::String, 
                         components_indexes::Dict{String, Vector{Int64}},
@@ -12,7 +33,7 @@ function fit_adalasso(Estimation_X::Matrix{Tl}, estimation_y::Vector{Fl}, α::Fl
         if key != "initial_states" && key != "μ₁"
             component = components_indexes[key]
             if key != "Exogenous_X" && key != "o" && !(key in ["ν₁", "γ₁"])
-                κ = count(i -> i == 0, coefs[component]) < 1 ? 0 : std(coefs[component])
+                κ = count(i -> i != 0, coefs[component]) < 1 ? 0 : std(coefs[component])
                 penalty_factor[component .- 1] .= (1 / (κ + adalasso_coef))
             else
                 penalty_factor[component .- 1]  = (1 ./ (abs.(coefs[component]) .+ adalasso_coef))
--- src/estimation_procedure/estimation_utils.jl
+++ src/estimation_procedure/estimation_utils.jl
@@ -1,3 +1,15 @@
+"""
+    get_dummy_indexes(Exogenous_X::Matrix{Fl}) where {Fl}
+
+    Identifies and returns the indexes of columns in the exogenous matrix that contain dummy variables.
+
+    # Arguments
+    - `Exogenous_X::Matrix{Fl}`: Exogenous variables matrix.
+
+    # Returns
+    - `Vector{Int}`: Vector containing the indexes of columns with dummy variables.
+
+"""
 function get_dummy_indexes(Exogenous_X::Matrix{Fl}) where{Fl}
     
     T, p = size(Exogenous_X)
@@ -12,6 +24,19 @@ function get_dummy_indexes(Exogenous_X::Matrix{Fl}) where{Fl}
     return dummy_indexes
 end
 
+"""
+    get_outlier_duplicate_columns(Estimation_X::Matrix{Tl}, components_indexes::Dict{String, Vector{Int64}}) where{Tl}
+
+    Identifies and returns the indexes of outlier columns that are duplicates of dummy variables in the exogenous matrix.
+
+    # Arguments
+    - `Estimation_X::Matrix{Tl}`: Matrix used for estimation.
+    - `components_indexes::Dict{String, Vector{Int64}}`: Dictionary containing indexes for different components.
+
+    # Returns
+    - `Vector{Int}`: Vector containing the indexes of outlier columns that are duplicates of dummy variables in the exogenous matrix.
+
+"""
 function get_outlier_duplicate_columns(Estimation_X::Matrix{Tl}, components_indexes::Dict{String, Vector{Int64}}) where{Tl}
     o_indexes = components_indexes["o"]
     exogenous_indexes = components_indexes["Exogenous_X"]
@@ -21,6 +46,27 @@ function get_outlier_duplicate_columns(Estimation_X::Matrix{Tl}, components_inde
     return o_indexes[dummy_indexes] .- 1
 end
 
+"""
+    fit_estimation_procedure(estimation_procedure::String, Estimation_X::Matrix{Tl}, estimation_y::Vector{Fl}, α::Float64,
+                             hyperparameter_selection::String, components_indexes::Dict{String, Vector{Int64}},
+                             adalasso_coef::Float64, select_exogenous::Bool)::Tuple{Vector{Float64}, Vector{Float64}} where {Tl, Fl}
+
+    Fits the specified estimation procedure (currently Lasso or AdaLasso) to the provided data and returns coefficients and residuals.
+
+    # Arguments
+    - `estimation_procedure::String`: The chosen estimation procedure (either "Lasso" or "AdaLasso").
+    - `Estimation_X::Matrix{Tl}`: Matrix of predictors for estimation.
+    - `estimation_y::Vector{Fl}`: Vector of response values for estimation.
+    - `α::Float64`: Elastic net control factor between ridge (α=0) and lasso (α=1) (default: 0.1).
+    - `hyperparameter_selection::String`: Information Criteria method for hyperparameter selection (default: aic).
+    - `components_indexes::Dict{String, Vector{Int64}}`: Dictionary containing indexes for different components.
+    - `adalasso_coef::Float64`: AdaLasso adjustment coefficient (default: 0.1).
+    - `select_exogenous::Bool`: Flag for selecting exogenous variables. When false the penalty factor for these variables will be set to 0.
+
+    # Returns
+    - `Tuple{Vector{Float64}, Vector{Float64}}`: Tuple containing coefficients and residuals of the fitted estimation procedure.
+
+"""
 function fit_estimation_procedure(estimation_procedure::String, Estimation_X::Matrix{Tl}, estimation_y::Vector{Fl}, α::Float64, 
                                     hyperparameter_selection::String, components_indexes::Dict{String, Vector{Int64}}, 
                                     adalasso_coef::Float64, select_exogenous::Bool)::Tuple{Vector{Float64}, Vector{Float64}} where {Tl, Fl}
