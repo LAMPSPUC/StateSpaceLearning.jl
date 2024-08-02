2dcd51e0aeb2f0072481aefa119584e966e0a15b
andre_ramos <andre_ramos+1-at-806357755127>
2024-08-02 16:46:10 -0400

    fix evaluation model

--- paper_tests/m4_test/evaluate_model.jl
+++ paper_tests/m4_test/evaluate_model.jl
@@ -8,7 +8,13 @@ function evaluate_SSL(initialization_df::DataFrame, results_df::DataFrame, input
 
     T= length(normalized_y)
     normalized_y = normalized_y[max(1, T-sample_size+1):end]
-    output = StateSpaceLearning.fit_model(normalized_y; estimation_input=Dict("α" => α, "information_criteria" => information_criteria, "ϵ" => 0.05, "penalize_exogenous" => true, "penalize_initial_states" => true),outlier=outlier,ζ_ω_threshold=12)
+    output = StateSpaceLearning.fit_model(normalized_y; 
+                                    model_input = Dict("stochastic_level" => true, "trend" => true, 
+                                            "stochastic_trend" => true, 
+                                             "seasonal" => true, "stochastic_seasonal" => true, "freq_seasonal" => 12,
+                                             "outlier" => outlier, "ζ_ω_threshold" => 12), 
+                                    estimation_input=Dict("α" => α, "information_criteria" => information_criteria, "ϵ" => 0.05, 
+                                    "penalize_exogenous" => true, "penalize_initial_states" => true))
     normalized_prediction = StateSpaceLearning.forecast(output, H)
     prediction = de_normalize(normalized_prediction, max_y, min_y)
 
